4faeb2b41406cbf2934eb8e72a55b52b621b21f2
Ulrich Schreiner <ulrich.schreiner@gmail.com>
2018-10-23 10:44:37 +0200

    enable Go modules for VSC

--- code.sh
+++ code.sh
@@ -18,5 +18,7 @@ echo 0 | code --install-extension waderyan.gitblame
 echo 0 | code --install-extension eamodio.gitlens
 
 export GOPATH=$WORKSPACE
+export GO111MODULE=on
+
 cd $WORKSPACE
 /usr/bin/code --verbose $DISABLE_GPU -p $WORKDIR
